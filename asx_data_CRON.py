0b92fefee4764f6f71f1feca62a8a69f0ddf7854
David Hume <djhume@gmail.com>
2013-08-03 13:33:03 +1200

    Update asx_data_CRON.py

--- asx_data_CRON.py
+++ asx_data_CRON.py
@@ -1,20 +1,6 @@
 '''
 ASX web scrapper - automatic monitoring of ASX futures market 
 
-Copyright (C) 2013 David Hume, Electricty Authority, New Zealand.
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 This is used to scrape the ASX data htm table into its separate sub tables.
 The subtables, since 14/7/2009 included the Benmore and Otahuhu AXS futures prices
